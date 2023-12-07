215765a4a7c56825bb68d3f421b198e0d3459bbd
marketneutral <jonathan.r.larkin@gmail.com>
2023-12-07 10:19:14 -0500

    function approximation for grid world works

--- q-learning.jl
+++ q-learning.jl
@@ -2,6 +2,7 @@ using POMDPs
 using POMDPModelTools: Deterministic
 using Random
 using Plots
+using LinearAlgebra: dot, norm
 
 struct GridWorldState
     x::Int64
@@ -45,8 +46,6 @@ function POMDPs.gen(m::GridWorld, s::GridWorldState, a::Symbol) #, rng::Abstract
     return (sp=GridWorldState(x, y, done), r=reward)
 end
 
-# Initialize the gridworld
-gridworld = GridWorld(5, 5, 10.0, -10.0, (3, 3))
 
 mutable struct QLearning
     S # state space (assumes 1:nstates)
@@ -61,10 +60,6 @@ function state_to_index(state::GridWorldState, size_x::Int64, size_y::Int64)
     return (state.y - 1) * size_x + state.x
 end
 
-function state_to_index(state::GridWorldState, size_x::Int64, size_y::Int64)
-    return (state.y - 1) * size_x + state.x
-end
-
 function action_to_index(action::Symbol)
     return Dict(:up => 1, :down => 2, :left => 3, :right => 4)[action]
 end
@@ -73,7 +68,11 @@ function index_to_action(index::Int64)
     return [:up, :down, :left, :right][index]
 end
 
-
+function index_to_state(index::Int64, size_x::Int64, size_y::Int64)
+    y = div(index - 1, size_x) + 1
+    x = index - (y - 1) * size_x
+    return GridWorldState(x, y, false)
+end
 
 
 function lookahead(model::QLearning, s, a)
@@ -90,7 +89,7 @@ end
 
 
 # Define the epsilon-greedy policy
-function π(model::QLearning, s, exploration::EpsilonGreedyExploration)
+function π(model, s, exploration::EpsilonGreedyExploration)
     if rand() < exploration.epsilon
         return rand(POMDPs.actions(gridworld)) # Explore: take a random action
     else
@@ -123,6 +122,8 @@ function simulate(P::MDP, model, π, h, s)
 end
 
 
+# Initialize the gridworld
+gridworld = GridWorld(5, 5, 10.0, -10.0, (3, 3))
 n_states = length(POMDPs.states(gridworld))
 n_actions = length(POMDPs.actions(gridworld))
 Q = zeros(n_states, n_actions)
@@ -136,6 +137,91 @@ s = POMDPs.initialstate(gridworld)
 # simulate fuction
 simulate(gridworld, model, (m, s) -> π(m, s, exploration), 100_000, s)
 
+
+# show the Q function values across a grid
+function print_Q_table(model)
+    Q_table = zeros(25, 4);
+    for i in 1:25
+        for j in 1:4
+            a = index_to_action(j)
+            s = index_to_state(i, 5, 5)
+            Q_table[i, j] = lookahead(model, s, a)
+        end
+    end
+    return Q_table
+end
+
+# show the optimal action for each state
+function print_optimal_policy(model)
+    policy = zeros(25, 1);
+    for i in 1:25
+        values = [lookahead(model, index_to_state(i, 5, 5), a) for a in POMDPs.actions(gridworld)]
+        policy[i] = argmax(values)
+    end
+    return policy
+end
+
+
+
+# Gradient-based Q-learning
+
+struct GradientQLearning
+    A # action space (assumes 1:nactions)
+    gamma # discount
+    Q # parameterized action value function Q(theta,s,a)
+    grad_Q # gradient of action value function
+    theta # action value function parameter
+    alpha # learning rate
+end
+
+scale_gradient(∇, L2_max) = min(L2_max/norm(∇), 1)*∇
+
+function lookahead(model::GradientQLearning, s, a)
+    return model.Q(model.theta, s, a)
+end
+
+function update!(model::GradientQLearning, s, a, r, s_prime)
+    A, gamma, Q, theta, alpha = model.A, model.gamma, model.Q, model.theta, model.alpha
+    u = maximum(Q(θ, s_prime, a_prime) for a_prime in A)
+    Δ = (r + gamma*u - Q(theta, s, a)) * model.grad_Q(theta, s, a)
+    theta[:] += alpha*scale_gradient(Δ, 1)
+    return model
+end
+
+
+#basis(s, a) = [s.x, s.y, s.x*s.y, s.x^2, s.y^2, a == :up, a == :down, a == :left, a == :right]
+
+function basis(s, a)
+    state_features = [s.x, s.y, s.x*s.y, s.x^2, s.y^2]
+    action_features = [a == :up, a == :down, a == :left, a == :right]
+    # all combinations of state and action features
+    combs =  [s_f * a_f for s_f in state_features, a_f in action_features]
+    return vcat(state_features, action_features, combs[:])
+end
+
+# s = GridWorldState(1, 1, false)
+# a = :up
+# length(basis(s, a))
+
+
+Q_func(θ, s, a) = dot(θ, basis(s, a))
+grad_Q_func(θ, s, a) = basis(s, a)
+
+# intialize the theta vector with unform random values between -1 and 1
+θ = 2 .* rand(29) .- 1
+alpha = 0.5
+
+model_gql = GradientQLearning(1:n_actions, 0.95, Q_func, grad_Q_func, θ, alpha)
+
+simulate(gridworld, model_gql, (m, s) -> π(m, s, exploration), 100_000, s)
+
+#print_Q_table(model_gql)
+print_optimal_policy(model_gql)
+
+
+
+
+# Vizualization
 
 
 function visualize_gridworld(gridworld::GridWorld, state::GridWorldState)
